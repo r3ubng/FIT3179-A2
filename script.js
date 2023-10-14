7725f042830b504467a784f8db3564faf7cae11f
r3ubng <142303545+r3ubng@users.noreply.github.com>
2023-10-15 02:43:33 +1100

    add A2 files

---
+++ script.js
@@ -0,0 +1,29 @@
+var vg_1 = "map.vg.json";
+vegaEmbed("#map", vg_1, {renderer: "svg", actions: false}).then(function (result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "bubblechart.vg.json";
+vegaEmbed("#bubbleChart", vg_2, {renderer: "svg", actions: false}).then(function (result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "stackedBar.vg.json";
+vegaEmbed("#barCharts", vg_3, {renderer: "svg", actions: false}).then(function (result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "pieChart1.vg.json";
+vegaEmbed("#pieChart1", vg_4, {renderer: "svg", actions: false}).then(function (result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "pieChart2.vg.json";
+vegaEmbed("#pieChart2", vg_5, {renderer: "svg", actions: false}).then(function (result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "heatmap.vg.json";
+vegaEmbed("#heatmap", vg_6, {renderer: "svg", actions: false}).then(function (result) {
+    // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
